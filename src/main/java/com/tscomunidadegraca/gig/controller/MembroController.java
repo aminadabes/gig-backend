eba69b639612c8d510fbfc46b46aaa0eb6bd12b6
Aminadabes Filho <amina.jr@hotmail.com>
2021-10-28 15:54:40 -0300

    Implementação da classe de mensagem da aplicação

--- src/main/java/com/tscomunidadegraca/gig/controller/MembroController.java
+++ src/main/java/com/tscomunidadegraca/gig/controller/MembroController.java
@@ -1,6 +1,7 @@
 package com.tscomunidadegraca.gig.controller;
 
 import com.tscomunidadegraca.gig.entity.Membro;
+import com.tscomunidadegraca.gig.message.MensagemResposta;
 import com.tscomunidadegraca.gig.service.MembroService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,51 +23,31 @@ public class MembroController {
 
     @GetMapping("/membros")
     @ResponseStatus(HttpStatus.OK)
-    public List<Membro> findAll(){
-        return membroService.findAll();
-        /*if(membroList.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public Object findAll() {
+        List<Membro> membroList = membroService.findAll();
+        if (membroList.isEmpty()) {
+            return MensagemResposta.builder().mensagem("SEM_REGISTRO").build();
         } else {
-            return new ResponseEntity<>(HttpStatus.OK);
-        }*/
+            return MensagemResposta.builder().mensagem(membroList.toString()).build();
+        }
     }
 
     @GetMapping("/membros/{id}")
-    public ResponseEntity<Membro> findById(@PathVariable(value = "id") Long id){
+    public Object findById(@PathVariable(value = "id") Long id) {
         Optional<Membro> membro = membroService.findById(id);
-        if(!membro.isPresent()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (!membro.isPresent()) {
+            return MensagemResposta.builder().mensagem("Membro não encontrado!!!").build();
         } else {
-            return new ResponseEntity<>(HttpStatus.OK);
+            return MensagemResposta.builder().mensagem(membro.get().getNome()).build();
         }
     }
 
     @PostMapping("/membros")
-    public ResponseEntity<Membro> saveLive(@RequestBody Membro membro) {
-        /*live.setRegistrationDate(LocalDateTime.now());*/
-        return new ResponseEntity<Membro>(membroService.save(membro), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public MensagemResposta save(@RequestBody Membro membro) {
+        Membro novoMembro = membroService.save(membro);
+        return MensagemResposta.builder().mensagem("O "+novoMembro.getNome() + " salvo com sucesso!!!").build();
     }
 
-    @DeleteMapping("/membros/{id}")
-    public ResponseEntity<?> deleteLive(@PathVariable(value="id") long id) {
-        Optional<Membro> liveO = membroService.findById(id);
-        if(!liveO.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }else {
-            membroService.delete(liveO.get());
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-    }
 
-    /*@PutMapping("/membros/{id}")
-    public ResponseEntity<Membro> updateLive(@PathVariable(value="id") long id,
-                                                   @RequestBody  Membro membro) {
-        Optional<Membro> liveO = membroService.findById(id);
-        if(!liveO.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }else {
-            Membro.setId(liveO.get().getId());
-            return new ResponseEntity<Membro>(membroService.save(Membro), HttpStatus.OK);
-        }
-    }*/
 }
---
+++ src/main/java/com/tscomunidadegraca/gig/message/MensagemResposta.java
@@ -0,0 +1,11 @@
+package com.tscomunidadegraca.gig.message;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MensagemResposta {
+
+    private String mensagem;
+}
